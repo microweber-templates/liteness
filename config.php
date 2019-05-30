56a69b5090a4debc838ba807854f5e5a9bba5b44
Petko Yovchevski <pyovchevski@gmail.com>
2019-05-30 16:01:02 +0300

    Incrementing version number to 1.2

--- config.php
+++ config.php
@@ -3,12 +3,9 @@
 $config = array();
 $config['name'] = "Liteness";
 $config['author'] = "Microweber";
-<<<<<<< HEAD
-$config['version'] = 1.1;
+$config['version'] = 1.2;
 $config['standalone_module_skins'] = true;
-=======
-$config['version'] = 1.1;
->>>>>>> dcd581e9c37381f15aa677e769a69ab333275d13
+
 //$config['is_hidden_from_install_screen'] = true;
 //$config['is_default'] = 1; //if you set this parameter the template will be selected on the install screen
 
